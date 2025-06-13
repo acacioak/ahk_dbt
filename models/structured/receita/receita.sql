b9119ae0e95e9e8e4a02cb664b50a8080c984e98
acacioak <acacio.komesu@clicksign.com>
2025-06-13 15:46:23 -0300

    structured da tabela pagamentos

---
+++ models/structured/receita/receita.sql
@@ -0,0 +1,18 @@
+-- models/example/clients/clients.sql
+
+{{ config(
+    alias = 'receita',
+    database = 'postgres',
+    schema = 'revenues',
+    materialized = 'table',
+    tags = ["structured", "revenues", "revenues"]) }}
+
+select
+    pagamento_id,
+    pedido_id,
+    "valor" as amount,
+    metodo_pagamento,
+    data_pagamento
+
+
+from {{ source('raw_data', 'pagamentos') }}
