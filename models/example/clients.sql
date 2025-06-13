a0520c311b984e3c96bae09cb9382f6d8373996f
acacioak <acacio.komesu@clicksign.com>
2025-06-13 14:14:53 -0300

    Initial commit do projeto

---
+++ models/example/clients.sql
@@ -0,0 +1,22 @@
+-- models/example/clients/clients.sql
+
+{{ config(
+    alias = 'clients',
+    database = 'postgres',
+    schema = 'clients',
+    materialized = 'table',
+    tags = ["refined", "clients", "clients"]) }}
+
+select
+    client_id,
+    subscription_id,
+    "status",
+    data_assinatura,
+    data_cancelamento
+
+from {{ source('raw_data', 'clientes') }} 
+
+
+
+
+
